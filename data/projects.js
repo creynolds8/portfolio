f13fe8e3671dd9a7620b2cdcd24987e0e8b8064c
creynolds8 <cameron8.cr@gmail.com>
2024-08-25 16:16:45 -0700

    update project information

--- data/projects.js
+++ data/projects.js
@@ -3,19 +3,19 @@ const projects = [
     id: 1,
     thumbnail: 'data/images/BS-home.png',
     title: 'BeeSecure',
-    description: 'Midterm project',
+    description: "BeeSecure is a password storage app that connects multiple users to a single organization. Each user connected to an organization can access the same account information for easy information sharing and access throughout an organization. Users can access, create, and edit account information like the username and password for each account.",
   },
   {
     id: 2,
     thumbnail: 'data/images/J-sold-out.png',
     title: 'Jungle',
-    description: 'Onboarding expr',
+    description: "Jungle is a project that was used to simulate a real-world scenario in which you only have a little time to learn a new language before having to start work on an existing project in that new language. This project involved working on every level of the existing Jungle app, from the database to front-end rendering or implementing a completely new feature. This project also provided the chance to experience bug-fixing in unfamiliar code and to create both unit and end-to-end tests for both new and existing code.",
   },
   {
     id: 3,
     thumbnail: 'data/images/Main Page w: expanded category.png',
     title: 'BudgetBuddy',
-    description: 'Final Project',
+    description: "BudgetBuddy is a household finance management app developed for the final project at Lighthouse Labs' Web Development Bootcamp. Users are able to add daily transactions and categorize thsese transactions by account, merchant, and expense type. User created budgets will adjust based on new input transactions as well as updating the account information provided by users.",
   },
   // 4 : {
   //   thumbnail: ,
--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -5,8 +5,8 @@ const ProjectListItem = ({ project }) => {
     <>
       <div className="project-box m-4 p-4">
         <div className="project-content">
-          <img className="project-thumbnail" src={project.thumbnail} alt="IMG" width="100%" />
-          <p>{project.title}</p>
+          <img className="project-thumbnail mb-2" src={project.thumbnail} alt="IMG" width="100%" />
+          <h4>{project.title}</h4>
           <p>{project.description}</p>
         </div>
       </div>
