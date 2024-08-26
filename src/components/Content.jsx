0c92397f73b455987977b1b022649e72c0ccfc32
creynolds8 <cameron8.cr@gmail.com>
2024-08-25 18:07:43 -0700

    add intro section with links

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,13 @@
 import '../styles/Content.scss'
 import ProjectList from './ProjectList';
+import Intro from './Intro'
 
 const Content = () => {
   
   return (
     <>
       <div className='content'>
+        <Intro />
         <ProjectList />
       </div>
     </>
---
+++ src/components/Intro.jsx
@@ -0,0 +1,31 @@
+import '../styles/Intro.scss'
+
+const Intro = () => {
+  return (
+    <div className="intro mx-4">
+      <div className="intro-title">
+        <div className="intro-name">
+          Hi, my name is Cameron.
+        </div>
+        <div className="intro-icons">
+          <a href="https://www.linkedin.com/in/cameronreynolds8">
+            <img className="intro-icon mx-2" src="/linkedin.svg" alt="Li" height="32" />
+          </a>
+          <a href="https://github.com/creynolds8">
+            <img className="intro-icon mx-2" src="/github.svg" alt="Gh" height="32" />
+          </a>
+          <a href="https://flowcv.com/resume/qhi0h146rw">
+            <img className="intro-icon mx-2" src="/resume.svg" alt="Rs" height="32" />
+          </a>
+        </div>
+      </div>
+      <div className="intro-details">
+        <p>
+          I am a newly minted Full-Stack Web Developer with a passion for building clean and easy to use web applications.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
