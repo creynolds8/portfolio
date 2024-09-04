2597ca44b8953bfec46b8d8b91fe00c09f9b8ffd
creynolds8 <cameron8.cr@gmail.com>
2024-09-04 11:01:17 -0700

    create data for badges

---
+++ data/badges.js
@@ -0,0 +1,28 @@
+const badges = [
+  {
+    id: 1,
+    icon: 'data/images/fullstack-icon.svg',
+    alt: 'fullstack',
+    blurb: "Full-Stack"
+  },
+  {
+    id: 2,
+    icon: 'data/images/react-icon.svg',
+    alt: 'reactjs',
+    blurb: "ReactJS"
+  },
+  {
+    id: 3,
+    icon: 'data/images/rails-icon.svg',
+    alt: 'rails',
+    blurb: "Ruby on Rails"
+  },
+  {
+    id: 4,
+    icon: 'data/images/design-icon.svg',
+    alt: 'design',
+    blurb: "UI / UX Design"
+  },
+]
+
+export default badges;
