504caedd1bd736d5d935d8b6db275aa5f5873a4c
creynolds8 <cameron8.cr@gmail.com>
2024-09-15 10:18:40 -0700

    add tech stack to projects

--- data/projects.js
+++ data/projects.js
@@ -4,7 +4,8 @@ const projects = [
     thumbnail: 'data/images/projects/BS-home.png',
     title: 'BeeSecure',
     description: "BeeSecure is a password storage app that connects multiple users to a single organization. Each user connected to an organization can access the same account information for easy information sharing and access throughout an organization. Users can access, create, and edit account information like the username and password for each account.",
-    repoLink: 'https://github.com/creynolds8/Midterm-Project'
+    repoLink: 'https://github.com/creynolds8/Midterm-Project',
+    techStack: ["#Javascript", "#REST", "#Express"]
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ const projects = [
     title: 'Jungle',
     description: "Jungle is a project that was used to simulate a real-world scenario in which you only have a little time to learn a new language before having to start work on an existing project in that new language. This project involved working on every level of the existing Jungle app, from the database to front-end rendering or implementing a completely new feature. This project also provided the chance to experience bug-fixing in unfamiliar code and to create both unit and end-to-end tests for both new and existing code.",
     repoLink: 'https://github.com/creynolds8/jungle-project',
+    techStack: ["#Ruby", "#Rails", "#Testing"]
   },
   {
     id: 3,
@@ -19,6 +21,7 @@ const projects = [
     title: 'BudgetBuddy',
     description: "BudgetBuddy is a household finance management app developed for the final project at Lighthouse Labs' Web Development Bootcamp. Users are able to add daily transactions and categorize thsese transactions by account, merchant, and expense type. User created budgets will adjust based on new input transactions as well as updating the account information provided by users.",
     repoLink: 'https://github.com/creynolds8/BudgetBuddy',
+    techStack: ["#Ruby", "#Rails", "#SASS"]
   },
   // 4 : {
   //   thumbnail: ,
--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -5,6 +5,12 @@ import { mouseEnter, mouseLeave, mouseMove } from "../javascript/mouseTrackingCa
 
 const ProjectListItem = ({ project }) => {
   const boundingRef = useRef(null);
+
+  const techStackComponents = project.techStack.map(a => {
+    return (
+      <p className="tech-item me-2">{a}</p>
+    )
+  })
   return (
     <>
       <div
@@ -18,15 +24,20 @@ const ProjectListItem = ({ project }) => {
         mouseMove(boundingRef, e);
       }}
       className="project-box bg-green dynamic-hover p-1">
-        <div className="inner-section p-2">
-          <div className="project-image">
-            <a className="repo-link" href={`${project.repoLink}`}>
-              <img src="data/images/icons/github-white.svg" alt="Gh" height="32" />
-            </a>
-            <img className="project-thumbnail mb-2" src={project.thumbnail} alt="IMG" width="100%" />
+        <div className="inner-section p-2 d-flex flex-column justify-content-between">
+          <div>
+            <div className="project-image">
+              <a className="repo-link" href={`${project.repoLink}`}>
+                <img src="data/images/icons/github-white.svg" alt="Gh" height="32" />
+              </a>
+              <img className="project-thumbnail mb-2" src={project.thumbnail} alt="IMG" width="100%" />
+            </div>
+            <h4>{project.title}</h4>
+            <p>{project.description}</p>
           </div>
-          <h4>{project.title}</h4>
-          <p>{project.description}</p>
+          <div className="d-flex">
+            {techStackComponents}
+          </div>
         </div>
       </div>
     </>
