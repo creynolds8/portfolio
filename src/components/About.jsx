082fb89087a19625d3fc6b16b94d6abe03582b67
creynolds8 <cameron8.cr@gmail.com>
2024-09-15 11:04:38 -0700

    simplify styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,9 @@
-import '../styles/About.scss'
-
 const About = () => {
   return (
     <>
       <a name="About"></a>
       <h2 className='header'>About:</h2>
-      <div className="about border1 my-4">
+      <div className="about border1 my-4 w-75">
         I enjoy creating easy to use and aesthetically pleasing websites. My goal is to create web applications that are intuitive to use while continuing to learn about web development and improve my skills.
       </div>
     </>
