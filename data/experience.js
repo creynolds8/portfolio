e66b95221eae27380f6da6d37d11e951fdbc51bc
creynolds8 <cameron8.cr@gmail.com>
2024-09-16 13:05:43 -0700

    add experience data

---
+++ data/experience.js
@@ -0,0 +1,88 @@
+const experience = [
+  {
+    id: 1,
+    label: "Lighthouse Labs",
+    subLabel: "Fullstack Web Development Bootcamp",
+    timeline: "Apr 2024 - July 2024",
+    icon: "/data/images/LHL-icon.png",
+    alt: "LHL",
+    timelineIcon: "/data/images/icons/badges/fullstack-icon.svg",
+    timelineAlt: "FS-Dev",
+    blurbs: [
+      {
+        id: 1,
+        blurb: "Experience in the full spectrum of web development. From setting up routes and configuring databases on the backend, to creating visually appealing and user-friendly dynamic frontends, and everything in-between.",
+      },
+      {
+        id: 2,
+        blurb: "Developed profiency in Javascript, ReactJS, HTML, and Bootstrap, allowing for a solid foundation of front development as well as design and styling.",
+      },
+      {
+        id: 3,
+        blurb: "Built backend servers from the ground up using CRUD implementation with database integration using a variety of languages or frameworks. These include but not limited to Express, Rails, and PostgreSQL.",
+      },
+      {
+        id: 4,
+        blurb: "Variety of learning situations including solo projects, pair programming, lectures, group projects, and projects that simulate onboarding to an existing project. Teaching included how to learn new languages, frameworks, or techniques through the use of official documentation and other public sources.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Personal Projects",
+    subLabel: "Continued Learning",
+    timeline: "July 2024 - Present",
+    icon: "/data/images/icons/elearning.svg",
+    alt: "CL",    
+    timelineIcon: "/data/images/icons/web-dev.svg",
+    timelineAlt: "WD", 
+    blurbs: [
+      {
+        id: 1,
+        blurb: "Increased depth of knowledge using common frameworks like Bootstrap to leverage preset styling to speed up development.",
+      },
+      {
+        id: 2,
+        blurb: "Goals to learn new languages, libraries, and frameworks for a wider range of knowledge and the skills to build constantly improving web applications. These include Tailwind, Typescript, and Python",
+      },
+      {
+        id: 3,
+        blurb: "Continue to integrate RESTful API's into web applications to add functionality and provide a more user-friendly experience.",
+      },
+      // {
+      //   id: 4,
+      //   blurb: "",
+      // },
+    ],
+  },
+  // {
+  //   id: 3,
+  //   label: "",
+  //   subLabel: "",
+  //   timeline: "",
+  //   icon: "",
+  //   alt: "",
+  //   timelineIcon: "",
+  //   timelineAlt: "",
+  //   blurbs: [
+  //     {
+  //       id: 1,
+  //       blurb: "",
+  //     },
+  //     {
+  //       id: 2,
+  //       blurb: "",
+  //     },
+  //     {
+  //       id: 3,
+  //       blurb: "",
+  //     },
+  //     {
+  //       id: 4,
+  //       blurb: "",
+  //     },
+  //   ],
+  // },
+];
+
+export default experience;
